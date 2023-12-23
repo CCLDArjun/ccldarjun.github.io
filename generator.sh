535d10616dd1972383289613e856f1abf995605d
arjun <ccldarjun@icloud.com>
2023-12-22 17:16:07 -0800

    proper navbar

--- generator.sh
+++ generator.sh
@@ -1,5 +1,5 @@
-python generate.py templates/index.jinja.html
-python generate.py templates/about.jinja.html
+python3 generate.py templates/index.jinja.html
+python3 generate.py templates/about.jinja.html
 
-python generate.py templates/blog/neovim_overview.jinja.html
+python3 generate.py templates/blog/neovim_overview.jinja.html
 
